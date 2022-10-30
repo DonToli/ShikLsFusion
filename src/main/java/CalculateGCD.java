42409f4447634e398e611d90f6dcd9e57d4a93f5
DonToli <strt87@yandex.ru>
2022-10-30 23:04:17 +0300

    Добавил взаимодействие LsFusion с java

---
+++ src/main/java/CalculateGCD.java
@@ -0,0 +1,28 @@
+import lsfusion.server.data.sql.exception.SQLHandledException;
+import lsfusion.server.language.ScriptingErrorLog;
+import lsfusion.server.language.ScriptingLogicsModule;
+import lsfusion.server.logics.action.controller.context.ExecutionContext;
+import lsfusion.server.logics.classes.ValueClass;
+import lsfusion.server.logics.property.classes.ClassPropertyInterface;
+import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
+
+import java.math.BigInteger;
+import java.sql.SQLException;
+
+public class CalculateGCD extends InternalAction {
+
+    public CalculateGCD(ScriptingLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
+    }
+
+    @Override
+    protected void executeInternal(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+        BigInteger b1 = BigInteger.valueOf((Integer)getParam(0, context));
+        BigInteger b2 = BigInteger.valueOf((Integer)getParam(1, context));
+        BigInteger gcd = b1.gcd(b2);
+        try {
+            findProperty("gcd[]").change(gcd.intValue(), context);
+        } catch (ScriptingErrorLog.SemanticErrorException ignored) {
+        }
+    }
+}
